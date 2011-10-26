8d28750bbf88947cd1fff59f781feb117ccb645d
davilla <davilla@4pi.com>
2011-10-26 12:46:14 -0400

    [aml] fixed, /usr/local/include does not exist, nor does /usr/share/aclocal

--- tools/amlogic/depends/depends.mk
+++ tools/amlogic/depends/depends.mk
@@ -4,7 +4,7 @@ export PREFIX=${XBMCPREFIX}
 export TARGETFS
 export RLINK_PATH=-Wl,-rpath-link,${TARGETFS}/lib -Wl,-rpath-link,${TARGETFS}/usr/lib
 
-export CFLAGS=-isystem${XBMCPREFIX}/include -isystem${SDKSTAGE}/include -isystem${SDKSTAGE}/usr/include -isystem${SDKSTAGE}/usr/local/include -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib ${RLINK_PATH}
+export CFLAGS=-isystem${XBMCPREFIX}/include -isystem${SDKSTAGE}/usr/include -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib ${RLINK_PATH}
 export CXXFLAGS=${CFLAGS}
 export CPPFLAGS=${CFLAGS}
 export LDFLAGS=${RLINK_PATH} -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib
@@ -16,6 +16,6 @@ export CXXCPP=${CXX} -E
 export RANLIB=${TOOLCHAIN}/bin/${HOST}-ranlib
 export STRIP=${TOOLCHAIN}/bin/${HOST}-strip
 export OBJDUMP=${TOOLCHAIN}/bin/${HOST}-objdump 
-export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${PREFIX}/share/aclocal-1.10 -I ${TARGETFS}/usr/share/aclocal
+export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${PREFIX}/share/aclocal-1.10 -I ${SDKSTAGE}/usr/share/aclocal
 export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${SDKSTAGE}/lib/pkgconfig:${SDKSTAGE}/usr/lib/pkgconfig
 export PATH:=${PREFIX}/bin:$(PATH):${TOOLCHAIN}/bin
