20268b3e906493f2c5386cd612feb5c8e2077dee
theuni <theuni-nospam-@xbmc.org>
2011-10-29 17:25:42 -0400

    [aml] deps: refactor depends.mk
    Using --sysroot makes everything easier

--- tools/amlogic/depends/depends.mk
+++ tools/amlogic/depends/depends.mk
@@ -1,21 +1,18 @@
 export HOST=arm-none-linux-gnueabi
 export BUILD=i686-linux
-export PREFIX=${XBMCPREFIX}
-export TARGETFS
-export RLINK_PATH=-Wl,-rpath-link,${TARGETFS}/lib -Wl,-rpath-link,${TARGETFS}/usr/lib
-
-export CFLAGS=-isystem${XBMCPREFIX}/include -isystem${SDKSTAGE}/usr/include -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib ${RLINK_PATH}
-export CXXFLAGS=${CFLAGS}
-export CPPFLAGS=${CFLAGS}
-export LDFLAGS=${RLINK_PATH} -L${TARGETFS}/usr -L${TARGETFS}/usr/lib -L${XBMCPREFIX}/lib
-export LD=${TOOLCHAIN}/bin/${HOST}-ld
-export AR=${TOOLCHAIN}/bin/${HOST}-ar
-export CC=${TOOLCHAIN}/bin/${HOST}-gcc
-export CXX=${TOOLCHAIN}/bin/${HOST}-g++
-export CXXCPP=${CXX} -E
-export RANLIB=${TOOLCHAIN}/bin/${HOST}-ranlib
-export STRIP=${TOOLCHAIN}/bin/${HOST}-strip
-export OBJDUMP=${TOOLCHAIN}/bin/${HOST}-objdump 
-export ACLOCAL=aclocal -I ${PREFIX}/share/aclocal -I ${PREFIX}/share/aclocal-1.10 -I ${SDKSTAGE}/usr/share/aclocal
-export PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig:${SDKSTAGE}/lib/pkgconfig:${SDKSTAGE}/usr/lib/pkgconfig
-export PATH:=${PREFIX}/bin:$(PATH):${TOOLCHAIN}/bin
+export PREFIX=$(XBMCPREFIX)
+export SYSROOT=$(BUILDROOT)/output/host/usr/arm-unknown-linux-gnueabi/sysroot
+export CFLAGS=-isystem$(PREFIX)/include
+export CXXFLAGS=$(CFLAGS)
+export CPPFLAGS=$(CFLAGS)
+export LDFLAGS=-L$(XBMCPREFIX)/lib
+export LD=$(TOOLCHAIN)/bin/$(HOST)-ld --sysroot=$(SYSROOT)
+export CC=$(TOOLCHAIN)/bin/$(HOST)-gcc --sysroot=$(SYSROOT)
+export CXX=$(TOOLCHAIN)/bin/$(HOST)-g++ --sysroot=$(SYSROOT)
+export OBJDUMP=$(TOOLCHAIN)/bin/$(HOST)-objdump
+export RANLIB=$(TOOLCHAIN)/bin/$(HOST)-ranlib
+export STRIP=$(TOOLCHAIN)/bin/$(HOST)-strip
+export AR=$(TOOLCHAIN)/bin/$(HOST)-ar
+export CXXCPP=$(CXX) -E
+export PKG_CONFIG_PATH=$(PREFIX)/lib/pkgconfig
+export PATH:=$(PREFIX)/bin:$(BUILDROOT)/output/host/usr/bin:$(PATH)
